fc49dbbcac272d7878f545c281f823aec05b316b
daniel-alt-pages <danielff999gf@gmail.com>
2026-01-12 18:06:12 -0500

    feat: Add comprehensive design improvements with better spacing and visual hierarchy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './additional-styles.css'
 import './mobile-styles.css'
+import './design-improvements.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
